9449e02679d2a6df0e8776b959808874b8b8a5da
cbacilio <dbacilio88@outlook.es>
2025-04-21 21:32:10 -0500

    feat(library): init-project

--- src/main/java/com/bacsystem/lib/constants/MongoConstant.java
+++
@@ -1,9 +0,0 @@
-package com.bacsystem.lib.constants;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class MongoConstant {
-
-    public final String FIELD_MONGO_COMPONENT_NAME = "name";
-}
